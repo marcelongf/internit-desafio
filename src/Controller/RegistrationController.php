f8c49491fdea1e1c81c9af9379f5575c57016828
Marcelo Nassar <marcelongf@gmail.com>
2020-01-29 13:56:27 -0200

    registro de usuarios feito

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -12,10 +14,18 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
+
+    private function alreadyRegistered(String $email, UserRepository $ur){
+        if($ur->findBy(array('email' => $email))){
+            return true;
+        }   
+        return false;
+    }
+
     /**
      * @Route("/register", name="register")
      */
-    public function index(Request $req, UserPasswordEncoderInterface $userEncoder){
+    public function index(Request $req, UserPasswordEncoderInterface $passwordEncoder, UserRepository $ur){
         //Cria o formulario de registro do novo usuario
         $form = $this->createFormBuilder()
             ->add('email')
@@ -37,6 +47,36 @@ class RegistrationController extends AbstractController
             ])
             ->getForm()
         ;
+
+        $form->handleRequest($req);
+        //Se o form ja tiver sido enviado, entra nesse bloco
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            
+            //precisamos checar se o email ja nao está cadastrado
+            if(!$this->alreadyRegistered($data['email'], $ur)){
+
+                //Aqui se checa se a senha é igual a confirmação
+                if(isset($data['password'])){
+                    $user = new User();
+                    $user->setEmail($data['email']);
+                    $user->setUsername($data['username']);
+                    $user->setPassword(
+                        $passwordEncoder->encodePassword($user, $data['password'])
+                    );
+                    $user->setAddress($data['address']);
+                    $user->setCity($data['city']);
+                    $user->setState($data['state']);
+                    $user->setCpf($data['cpf']);
+
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($user);
+                    $em->flush();
+
+                    return $this->redirect($this->generateUrl('app_login'));
+                }
+            }
+        }
 
 
         return $this->render('registration/index.html.twig', [
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
+ * @UniqueEntity(fields={"email"}, message="Email already exists")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements UserInterface
